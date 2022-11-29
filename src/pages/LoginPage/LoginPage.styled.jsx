74b400b39cb89596c09923166102ab2c55ceb195
Ruslan Babaiev <ruslan.nadirovich@gmail.com>
2022-11-29 16:33:14 +0200

    removed unused code

--- src/pages/LoginPage/LoginPage.styled.jsx
+++ src/pages/LoginPage/LoginPage.styled.jsx
@@ -1,16 +1,6 @@
 import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 
-export const FormTitle = styled.h1``;
-
-export const Form = styled.form``;
-
-export const Label = styled.label``;
-
-export const Input = styled.input``;
-
-export const Submit = styled.button``;
-
 export const Link = styled(NavLink)`
   color: ${p => p.theme.palette.primary.dark};
 `;
